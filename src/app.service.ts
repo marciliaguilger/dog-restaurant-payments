c1767a13bf671f2143c88a2b7c9fd5fccc2cb762
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2024-11-16 16:02:07 -0300

    create

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosResponse } from 'axios';
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<string> {
+    const url = 'http://hello-service.default.svc.cluster.local/hello';
+    return fetchData(url)
+  }
+}
+
+// Defina a interface para o tipo de dados que você espera da resposta
+interface ApiResponse {
+  data: string; 
+}
+
+// Função para fazer a chamada HTTP
+async function fetchData(url: string): Promise<string> {
+  try {
+    const response: AxiosResponse<string> = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error('Error message: ', error.message);
+      throw new Error('Failed to fetch data');
+    } else {
+      console.error('Unexpected error: ', error);
+      throw new Error('An unexpected error occurred');
+    }
   }
 }
