b7c37200395956c00e3c895d3059ad5544fc5106
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2024-12-01 17:54:52 -0300

    update

--- src/frameworks-drivers/api/pagamento.controller.ts
+++ src/frameworks-drivers/api/pagamento.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Inject, Post } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
+import { Pagamento } from "src/domain/entities/pagamento.entity";
 import { IPagamentoUseCase } from "src/domain/use-cases/pagamento-use-case.interface";
-import { TipoPagamento } from "src/domain/value-objects/tipo-pagamento";
 import { CreatePagamentoInput } from "src/interface-adapters/presenters/api-model/inputs/create-pagamento.input";
 
 @ApiTags('Pagamento')
@@ -12,7 +12,7 @@ export class PagamentoController{
         private readonly pagamentoUseCase: IPagamentoUseCase) { }
 
     @Post()
-    async createPagamento(@Body() input: CreatePagamentoInput) {
-        this.pagamentoUseCase.create(input.clienteId, input.pedidoId, input.tipoPagamento)
+    async createPagamento(@Body() input: CreatePagamentoInput): Promise<Pagamento> {
+        return this.pagamentoUseCase.create(input.clienteId, input.pedidoId, input.tipoPagamento)
     }
 }
